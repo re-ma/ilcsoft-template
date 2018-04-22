34e7f6e2494d12225d52f5ec710dc0856fffe71e
re-ma <re-ma@users.noreply.github.com>
2018-04-23 01:32:08 +0900

    change Sc xml ecal digitisation paramter from ealisticCaloDigiSilicon to RealisticCaloDigiScinPpd

--- ddMarlin/ddMarlin_run.sh
+++ ddMarlin/ddMarlin_run.sh
@@ -2,6 +2,8 @@
 
 source /cvmfs/ilc.desy.de/sw/x86_64_gcc49_sl6/v01-19-06/init_ilcsoft.sh
 
+convertToGear default $lcgeo_DIR/ILD/compact/ILD_l5_v02/ILD_l5_v02.xml gear_ILD_l5_v02_dd4hep.xml
+
 DataDIR="/home/ilc/reima/hybrid-ECAL/dd4hep/ILD_l5_v02/mu-10GeV/data/"
 
 Marlin ddMarlin_Sc.xml \
