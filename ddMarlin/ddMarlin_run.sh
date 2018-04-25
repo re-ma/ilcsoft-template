ef0fc852436963965f5595eeabc68c276b6ec2e5
re-ma <re-ma@users.noreply.github.com>
2018-04-25 16:15:25 +0900

    change detector model from ILD_L5_v2 to ILD_l5_o1_v2

--- ddMarlin/ddMarlin_run.sh
+++ ddMarlin/ddMarlin_run.sh
@@ -2,17 +2,17 @@
 
 source /cvmfs/ilc.desy.de/sw/x86_64_gcc49_sl6/v01-19-06/init_ilcsoft.sh
 
-convertToGear default $lcgeo_DIR/ILD/compact/ILD_l5_v02/ILD_l5_v02.xml gear_ILD_l5_v02_dd4hep.xml
+convertToGear default $lcgeo_DIR/ILD/compact/ILD_l5_o1_v02/ILD_l5_o1_v02.xml gear_ILD_l5_o1_v02_dd4hep.xml
 
 DataDIR="/home/ilc/reima/hybrid-ECAL/dd4hep/ILD_l5_v02/mu-10GeV/data/"
 
 Marlin ddMarlin_Sc.xml \
 --global.LCIOInputFiles="$DataDIR/mu-10_00.slcio $DataDIR/mu-10_01.slcio $DataDIR/mu-10_02.slcio $DataDIR/mu-10_03.slcio $DataDIR/mu-10_04.slcio $DataDIR/mu-10_05.slcio $DataDIR/mu-10_06.slcio $DataDIR/mu-10_07.slcio $DataDIR/mu-10_08.slcio $DataDIR/mu-10_09.slcio" \
---global.GearXMLFile=gear_ILD_l5_v02_dd4hep.xml \
---InitDD4hep.DD4hepXMLFile=$lcgeo_DIR/ILD/compact/ILD_l5_v02/ILD_l5_v02.xml
+--global.GearXMLFile=gear_ILD_l5_o1_v02_dd4hep.xml \
+--InitDD4hep.DD4hepXMLFile=$lcgeo_DIR/ILD/compact/ILD_l5_v02/ILD_l5_o1_v02.xml
 
 Marlin lctuple_Sc.xml \
---global.GearXMLFile=gear_ILD_l5_v02_dd4hep.xml
+--global.GearXMLFile=gear_ILD_l5_o1_v02_dd4hep.xml
 
 ###Sample
 #--global.LCIOInputFiles="" \
