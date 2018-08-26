5ab276fb79e6e27539a4ad351d1910a9b664946c
re-ma <reima.tokyo@gmail.com>
2018-08-27 01:01:07 +0900

    first version script for v02-00-01

--- ddMarlin/ddMarlin_batch.sh
+++ ddMarlin/ddMarlin_batch.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-make
-DataDIR="$HOME/group-folder/"`pwd`"/data"
-mkdir -p $DataDIR
-ln -s $DataDIR data
-ls -lt data
-
 ## param setting
 #FNDIR="/home/ilc/reima/hybrid-ECAL/dd4hep/3rd_ILD_l5_v02/uds91/data"
 FNDIR="/home/ilc/reima/hybrid-ECAL/dd4hep/3rd_ILD_l5_v02/gamma10GeV/data"
 #FNPrefix="E1-calib.Puds91.Gsgreen.e0.p0.I110048."
 FNPrefix="ILD_l5_v02_E1-calib.Ps_22_p010.Glcio_gun.e0.p0.I110141.n1.slcio"
-EventPrefix=""
+EventPrefix="test-"
 BName="g10"
 BJob="s"
 
-Num=0
-#Num=1
+Num=0 #0 for single file
+#Num=1 #1 for 2 or more files, if you have 2 or more files, remove comennt out
 EndNum=11
+
+## Start this script
+if [ -e data ]; then
+    echo "OK, data directory is exist"
+else
+    echo "data directory is NOT found"
+    echo 'please run "sh ddMarlin_pre.sh"'
+    exit 1
+fi
+
+if [ -e logs ]; then
+    echo "OK, logs directory is exist"
+else
+    echo "logs directory is NOT found"
+    echo 'please run "sh ddMarlin_pre.sh"'
+    exit 1
+fi
+
 
 if [ "$Num" = "0" ]; then
 
--- ddMarlin/ddMarlin_pre.sh
+++ ddMarlin/ddMarlin_pre.sh
@@ -3,26 +3,29 @@
 DataDIR="$HOME/group-folder/"`pwd`"/data"
 
 if [ -e data ]; then
-
+    echo "OK, data directory is exist"
 else;
     mkdir -p $DataDIR
     ln -s $DataDIR data
     ls -lt data
+    echo "make data directory"
 fi
 
-mkdir logs
+if [ -e logs ]; then
+    echo "OK, data directory is exist"
+else;
+    mkdir logs
+    echo "make logs directory"
+fi
 
-ILDConfig_DIR="/cvmfs/ilc.desy.de/sw/ILDConfig/v01-19-06-p01/StandardConfig/lcgeo_current/"
+ILDConfig_DIR="/cvmfs/ilc.desy.de/sw/ILDConfig/v02-00-01/StandardConfig/production"
 
-cp $ILDConfig_DIR/LikelihoodPID_* .
-cp $ILDConfig_DIR/PandoraLikelihoodData* .
-cp $ILDConfig_DIR/PandoraSettings* .
-cp -r $ILDConfig_DIR/weightFiles_forLowMomentumMuPiSeparation .
-
-source /cvmfs/ilc.desy.de/sw/x86_64_gcc49_sl6/v01-19-06/init_ilcsoft.sh
-
-#convertToGear default $lcgeo_DIR/ILD/compact/ILD_l5_o1_v02/ILD_l5_o1_v02.xml gear_ILD_l5_o1_v02_dd4hep.xml
-convertToGear default $lcgeo_DIR/ILD/compact/ILD_l5_v02/ILD_l5_v02.xml gear_ILD_l5_v02_dd4hep.xml
-
-#!/bin/bash
-
+cp $ILDConfig_DIR/MarlinStdReco.xml
+cp -r $ILDConfig_DIR/Calibration .
+cp -r $ILDConfig_DIR/CaloDigi .
+cp -r $ILDConfig_DIR/Config .
+cp -r $ILDConfig_DIR/HighLevelReco .
+cp -r $ILDConfig_DIR/PandoraSettings .
+cp -r $ILDConfig_DIR/ParticleFlow .
+cp -r $ILDConfig_DIR/Tracking .
+cp $ILDConfig_DIR/MarlinStdRecoLCTuple.xml
